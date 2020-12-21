0b377791d9a29f098f8986880f634a06985d92c9
Nguyen Tran <nguyentran4896@gmail.com>
2020-12-21 23:45:58 +0700

    Init UI header

---
+++ src/components/header/header.js
@@ -0,0 +1,36 @@
+import { Component } from 'react';
+import {
+  Navbar,
+  Form,
+  FormControl,
+  Button
+} from 'react-bootstrap';
+
+export default class Header extends Component {
+  render() {
+    return (
+      <div className="header">
+        <Navbar bg="dark" expand="lg" variant="dark">
+          <Navbar.Brand href="#home">
+            <img
+              src="/logo192.png"
+              width="30"
+              height="30"
+              className="d-inline-block align-top"
+              alt="React Bootstrap logo"
+            />
+            {' '}Funny Movies
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav" style={{ flexDirection: 'row-reverse' }}>
+            <Form inline>
+              <FormControl type="text" placeholder="Email" className="mr-sm-2" />
+              <FormControl type="text" placeholder="Password" className="mr-sm-2" />
+              <Button variant="outline-success">Login</Button>
+            </Form>
+          </Navbar.Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+}
