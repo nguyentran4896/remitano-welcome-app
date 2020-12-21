bbf394d1fda6d31fb29b0069a5ac459cc4e2081b
Nguyen Tran <nguyentran4896@gmail.com>
2020-12-22 00:15:18 +0700

    Fixed styling code

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -30,6 +30,7 @@ export default class Header extends Component {
               <Button variant="outline-success">Login</Button>
             </Form>
           </Navbar.Collapse>
+          <hr/>
         </Navbar>
       </div>
     );
--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,14 +1,14 @@
 import './App.css';
-import { Component } from 'react';
-import Header from '../../components/header/header'
+import React, {Component} from 'react';
+import Header from '../../components/header/header';
 
 class App extends Component {
   render() {
     return (
-      < div className="App" >
-          <Header />
-      </div >
-    )
+      <div className="App">
+        <Header />
+      </div>
+    );
   }
 }
 
